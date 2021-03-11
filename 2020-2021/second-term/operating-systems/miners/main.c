e31fa9fe18d9b6551870b85a06766a448c5b3d63
krispetrov <petrov.kris03@gmail.com>
2021-03-11 17:59:47 +0200

    Fixed some issues with sleep and number

--- 2020-2021/second-term/operating-systems/miners/main.c
+++ 2020-2021/second-term/operating-systems/miners/main.c
@@ -5,11 +5,12 @@
 
 pthread_mutex_t mutex;
 
-static int miners = 1;
-static int traders = 1;
+static int miners = 3;
+static int traders = 3;
 static int storage = 0;
 
 void* mine(void* arg) {
+    int number = *(int*)(arg) + 1;
     for(int i = 0; i < 20; i++) {
         if (pthread_mutex_lock(&mutex) != 0) {
             perror("pthread_mutex_lock");
@@ -17,19 +18,20 @@ void* mine(void* arg) {
         }
 
         storage+=10;
-        printf("Miner %d gathered 10 gold\n", *(int*)(arg));
-        sleep(2);
+        printf("Miner %d gathered 10 gold\n", number);
 
         if (pthread_mutex_unlock(&mutex) != 0) {
             perror("pthread_mutex_lock");
             return NULL;
         }
+        sleep(2);
     }
 
     return NULL;
 }
 
 void* trade(void* arg) {
+    int number = *(int*)(arg) + 1;
     for(int i = 0; i < 20; i++) {
         if (pthread_mutex_lock(&mutex) != 0) {
             perror("pthread_mutex_lock");
@@ -39,22 +41,22 @@ void* trade(void* arg) {
         if(storage == 0) {
             printf("The warehouse is empty, cannot sell!\n");
         }
-        else if (storage >= 10){
+        else {
             storage-=10;
-            printf("Trader %d sold 10 gold\n", *(int*)(arg));
-            sleep(2);
+            printf("Trader %d sold 10 gold\n", number);
         }
 
         if (pthread_mutex_unlock(&mutex) != 0) {
             perror("pthread_mutex_lock");
             return NULL;
         }
+        sleep(2);
     }
 
     return NULL;
 }
 
-int main(void) {
+int main(int argc, char* argv[]) {
     if(pthread_mutex_init(&mutex, NULL) != 0) {
         perror("pthread_mutex_init");
         return -1;
@@ -64,14 +66,14 @@ int main(void) {
     pthread_t *traders_group = malloc(sizeof(pthread_t) * traders);
 
     for(int i = 0; i < miners; i++) {
-        if(pthread_create(&miners_group[i], NULL, mine, NULL) != 0) {
+        if(pthread_create(&miners_group[i], NULL, mine, &i) != 0) {
             perror("pthread_create");
             return -1;
         }
     }
 
     for(int i = 0; i < traders; i++) {
-        if(pthread_create(&traders_group[i], NULL, trade, NULL) != 0) {
+        if(pthread_create(&traders_group[i], NULL, trade, &i) != 0) {
             perror("pthread_create");
             return -1;
         }
@@ -91,10 +93,10 @@ int main(void) {
         }
     }
 
-    printf("Gold: %d\n", storage);
     if(pthread_mutex_destroy(&mutex) != 0) {
         perror ("pthread_mutex_destroy");
     }
+    printf("Gold: %d\n", storage);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
