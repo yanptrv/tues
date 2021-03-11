1c8fa31db6f27891c0e9ef15d3de5f642dad3688
krispetrov <petrov.kris03@gmail.com>
2021-03-12 00:13:57 +0200

    Created threads

--- 2020-2021/second-term/operating-systems/dealers/main.c
+++ 2020-2021/second-term/operating-systems/dealers/main.c
@@ -1,6 +1,47 @@
+#include <pthread.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+#define TEST_DRIVERS 20
+#define CARS 5
+
+pthread_mutex_t mutex;
+static int times_driven = 0;
+
+void* test_drive(void* arg) {
+    if(pthread_mutex_lock(&mutex) != 0) {
+        perror("pthread_mutex_lock");
+    }
+
+
+    if (pthread_mutex_unlock(&mutex) != 0) {
+        perror("pthread_mutex_unlock");
+    }
+
+
+    return NULL;
+}
+
+int main(void) {
+    if(pthread_mutex_init(&mutex, NULL) != 0) {
+        perror("pthread_mutex_init");
+    }
+    pthread_t *drivers_group = malloc(sizeof(pthread_t) * TEST_DRIVERS);
+
+    for(int i = 0; i < TEST_DRIVERS; i++) {
+        if(pthread_create(&drivers_group[i], NULL, test_drive, NULL) != 0) {
+            perror("pthread_create");
+        }
+    }
+
+    for(int i = 0; i < TEST_DRIVERS; i++) {
+        if(pthread_join(drivers_group[i], NULL) != 0) {
+            perror("pthread_join");
+        }
+    }
+    if(pthread_mutex_destroy(&mutex) != 0) {
+        perror("pthread_mutex_destroy");
+    }
+
     return 0;
 }
