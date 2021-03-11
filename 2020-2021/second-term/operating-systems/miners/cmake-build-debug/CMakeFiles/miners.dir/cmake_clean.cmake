7c8a303ea65579dc6b60608e9f8a0f2b4e7b6716
krispetrov <petrov.kris03@gmail.com>
2021-03-11 16:47:15 +0200

    Initial commit

---
+++ 2020-2021/second-term/operating-systems/miners/cmake-build-debug/CMakeFiles/miners.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miners.dir/main.c.o"
+  "miners"
+  "miners.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/miners.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2020-2021/second-term/operating-systems/miners/cmake-build-debug/CMakeFiles/miners.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for miners.
+# This may be replaced when dependencies are built.
