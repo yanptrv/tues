dbeaa884f15293d16900b321659098fc2ff8b5d2
krispetrov <petrov.kris03@gmail.com>
2021-03-13 14:35:26 +0200

    Array mutex

--- 2020-2021/second-term/operating-systems/starcraft-3/main.c
+++ 2020-2021/second-term/operating-systems/starcraft-3/main.c
@@ -1,24 +1,31 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int scv = 5;
-int minerals = 0;
+int minerals_in_storage = 0;
 int soldiers = 0;
 
-pthread_mutex_t mutex;
+pthread_mutex_t *mutex;
 
 void* get_minerals(void* arg) {
-    for(;;) {
-        if (pthread_mutex_trylock(&mutex) == 0) {
+    long minerals_counter = (long)arg;
+    for(int i = 0; i < minerals_counter; i++) {
+        if (pthread_mutex_trylock(&mutex[i]) == 0) {
 
+            printf("Moving to mineral block");
+            sleep(3);
 
-
-            if (pthread_mutex_unlock(&mutex) != 0) {
+            if (pthread_mutex_unlock(&mutex[i]) != 0) {
                 perror("pthread_mutex_unlock");
                 return NULL;
             }
-        } else { ;
+        }
+        else {
+            if (i == minerals_counter-1) {
+                i = 0;
+            }
         }
     }
 
@@ -26,7 +33,9 @@ void* get_minerals(void* arg) {
 }
 
 int main(int argc, char* argv[]) {
-    if(pthread_mutex_init(&mutex, NULL) != 0) {
+
+    long minerals_count = (long) strtol(argv[1], (char**)NULL, 10);
+    if(pthread_mutex_init(mutex = malloc(sizeof(pthread_mutex_t)*minerals_count), NULL) != 0) {
         perror("pthread_mutex_init");
         return -1;
     }
@@ -34,7 +43,7 @@ int main(int argc, char* argv[]) {
     pthread_t *scv_group = malloc(sizeof(pthread_t) * scv);
 
     for(int i = 0; i < scv; i++) {
-        if(pthread_create(&scv_group[i], NULL, get_minerals, NULL) != 0) {
+        if(pthread_create(&scv_group[i], NULL, get_minerals, (void*)minerals_count) != 0) {
             perror("pthread_create");
             return -1;
         }
@@ -47,9 +56,11 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if(pthread_mutex_destroy(&mutex) != 0) {
-        perror("pthread_mutex_destroy");
-        return -1;
+    for(int i = 0; i < minerals_count; i++) {
+        if(pthread_mutex_destroy(&mutex[i]) != 0) {
+            perror("pthread_mutex_destroy");
+            return -1;
+        }
     }
 
     return 0;
