3cb2877fe3083bd56e6056bcbbf01e9d3d781ab0
krispetrov <petrov.kris03@gmail.com>
2021-03-16 23:14:55 +0200

    Added getters for x and y

--- 2020-2021/second-term/object-oriented-programming/geofigures-collision/src/com/home/Kris/Shape.java
+++ 2020-2021/second-term/object-oriented-programming/geofigures-collision/src/com/home/Kris/Shape.java
@@ -3,6 +3,15 @@ package com.home.Kris;
 public class Shape {
     private double x;
     private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
     Point center = new Point(x, y);
 
     public Shape(double x, double y) {
