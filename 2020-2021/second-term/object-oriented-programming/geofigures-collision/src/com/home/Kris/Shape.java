d60e6966e39e2694a5532127dae537824bfe5ae9
krispetrov <petrov.kris03@gmail.com>
2021-03-16 22:32:40 +0200

    Shape class containing a center

---
+++ 2020-2021/second-term/object-oriented-programming/geofigures-collision/src/com/home/Kris/Shape.java
@@ -0,0 +1,12 @@
+package com.home.Kris;
+
+public class Shape {
+    private double x;
+    private double y;
+    Point center = new Point(x, y);
+
+    public Shape(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
