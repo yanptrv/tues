b12042ea776ff87f70e5e379fc673b506563cd71
krispetrov <petrov.kris03@gmail.com>
2021-03-16 23:15:26 +0200

    Using the getters from Shape class for x and y

--- 2020-2021/second-term/object-oriented-programming/geofigures-collision/src/com/home/Kris/Square.java
+++ 2020-2021/second-term/object-oriented-programming/geofigures-collision/src/com/home/Kris/Square.java
@@ -14,10 +14,10 @@ public class Square extends Shape implements PointsList, Collision{
     @Override
     public List<Point> getPoints() {
         List<Point> points = new ArrayList<>();
-        Point one = new Point(center.getX()+d, center.getY()+d);
-        Point two = new Point(center.getX()+d, center.getY()-d);
-        Point three = new Point(center.getX()-d, center.getY()-d);
-        Point four = new Point(center.getX()-d, center.getY()+d);
+        Point one = new Point(getX()+this.d, getY()+this.d);
+        Point two = new Point(getX()+this.d, getY()-this.d);
+        Point three = new Point(getX()-this.d, getY()-this.d);
+        Point four = new Point(getX()-this.d, getY()+this.d);
         points.add(one);
         points.add(two);
         points.add(three);
@@ -28,10 +28,10 @@ public class Square extends Shape implements PointsList, Collision{
     @Override
     public boolean checkCollision(Shape other) {
         for(Point point : this.getPoints()) {
-            if(other.center.getX()-this.d <= point.getX()) {
-                if(point.getX() <= other.center.getX()+this.d) {
-                    if(other.center.getY()-this.d <= point.getY()) {
-                        return point.getY() <= other.center.getY() + this.d;
+            if(other.getX()-this.d <= point.getX()) {
+                if(point.getX() <= other.getX()+this.d) {
+                    if(other.getY()-this.d <= point.getY()) {
+                        return point.getY() <= other.getY() + this.d;
                     }
                     else { return false; }
                 }
