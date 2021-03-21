2fe10211513c3c6f881d76193dd19611be6ddf6b
krispetrov <petrov.kris03@gmail.com>
2021-03-21 19:33:11 +0200

    Added mineral blocks to mine from

--- 2020-2021/second-term/operating-systems/starcraft-3/main.c
+++ 2020-2021/second-term/operating-systems/starcraft-3/main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int scv = 5;
-int minerals_in_storage = 0;
+int minerals_in_storage = 0, *mineralBlocks;
 int soldiers = 0;
 long minerals_count;
 
@@ -12,44 +12,52 @@ pthread_mutex_t *mutex;
 
 void* get_minerals(void* arg) {
     long scv_number = (long)arg;
-    for(int i = 0; i < minerals_count; i++) {
-        sleep(3);
-        if (pthread_mutex_trylock(&mutex[i]) == 0) {
+    while(1) {
+        for (int i = 0; i < minerals_count; i++) {
+            sleep(3);
+            if (pthread_mutex_trylock(&mutex[i]) == 0) {
 
-            printf("SCV %ld is mining from mineral block %d\n", scv_number+1, i+1);
-            printf("SCV %ld is transporting minerals\n", scv_number+1);
-            sleep(2);
-            printf("SCV %ld delivered minerals to the Command center\n", scv_number+1);
-            minerals_in_storage+=8;
-
-            if (pthread_mutex_unlock(&mutex[i]) != 0) {
-                perror("pthread_mutex_unlock");
-                return NULL;
-            }
-            break;
-        }
-        else {
-            if (i == minerals_count-1) {
+                printf("SCV %ld is mining from mineral block %d\n", scv_number + 1, i + 1);
+                if (mineralBlocks[i] < 8) {
+                    minerals_in_storage += mineralBlocks[i];
+                    mineralBlocks = 0;
+                    break;
+                } else {
+                    mineralBlocks[i] -= 8;
+                    printf("%d\n", mineralBlocks[i]);
+                }
+                printf("SCV %ld is transporting minerals\n", scv_number + 1);
                 sleep(2);
-                i = 0;
+                minerals_in_storage += 8;
+                printf("SCV %ld delivered minerals to the Command center\n", scv_number + 1);
+
+                if (pthread_mutex_unlock(&mutex[i]) != 0) {
+                    perror("pthread_mutex_unlock");
+                    return NULL;
+                }
+            } else {
+                if (i == minerals_count - 1) {
+                    i = 0;
+                }
             }
         }
     }
-
-    return NULL;
 }
-
 int main(int argc, char* argv[]) {
 
     if(argc == 2) {
-        minerals_count = (long) strtol(argv[1], (char**)NULL, 10);
+        minerals_count = strtol(argv[1], (char**)NULL, 10);
     }
     else if(argc == 1){
         minerals_count = 2;
     }
 
-    mutex  = malloc(sizeof(pthread_mutex_t)*minerals_count);
+    mineralBlocks = malloc(sizeof(long) * minerals_count);
+    for(int i = 0; i < minerals_count; i++) {
+        mineralBlocks[i] = 500;
+    }
 
+    mutex = malloc(sizeof(pthread_mutex_t) * minerals_count);
     for(int i = 0; i < minerals_count; i++) {
         if(pthread_mutex_init(&mutex[i], NULL) != 0) {
             perror("pthread_mutex_init");
@@ -79,6 +87,8 @@ int main(int argc, char* argv[]) {
             return -1;
         }
     }
+    printf("%d", minerals_in_storage);
+    free(mineralBlocks);
     free(scv_group);
     free(mutex);
 
