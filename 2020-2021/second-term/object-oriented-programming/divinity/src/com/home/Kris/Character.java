2debd198fc6dbd1bf2672d463f26e112b38d7198
krispetrov <petrov.kris03@gmail.com>
2021-04-10 17:15:44 +0300

    Added info return and setters

--- 2020-2021/second-term/object-oriented-programming/divinity/src/com/home/Kris/Character.java
+++ 2020-2021/second-term/object-oriented-programming/divinity/src/com/home/Kris/Character.java
@@ -1,4 +1,30 @@
 package com.home.Kris;
 
-public class Character {
+public abstract class Character {
+    private int currentHealth;
+    private int maxHealth;
+    private int intelligence;
+    private int dexterity;
+
+    abstract protected String getCharacterClass();
+
+    public String toString() {
+        return "Class: " + getCharacterClass() + " Current HP: " + currentHealth + "\n";
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
 }
