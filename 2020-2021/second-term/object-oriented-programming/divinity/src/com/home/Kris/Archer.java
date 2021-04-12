719be2c69802abe5568b8b91f3b958e62558a4d6
krispetrov <petrov.kris03@gmail.com>
2021-04-12 09:34:01 +0300

    Character class and its roles

--- 2020-2021/second-term/object-oriented-programming/divinity/src/com/home/Kris/Archer.java
+++ 2020-2021/second-term/object-oriented-programming/divinity/src/com/home/Kris/Archer.java
@@ -6,9 +6,9 @@ public class Archer extends Character {
         return "Archer";
     }
 
-    public Archer() {
-        setCurrentHealth(90);
+    public Archer() throws Exception {
         setMaxHealth(90);
+        setCurrentHealth(90);
         setDexterity(20);
         setIntelligence(10);
     }
