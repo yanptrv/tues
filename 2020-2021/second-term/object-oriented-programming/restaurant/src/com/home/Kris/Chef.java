98b14fa00ab26ad5ffb09ee59c81e0f3fbda8e42
krispetrov <petrov.kris03@gmail.com>
2021-06-14 17:39:42 +0300

    Making classes runnable

--- 2020-2021/second-term/object-oriented-programming/restaurant/src/com/home/Kris/Chef.java
+++ 2020-2021/second-term/object-oriented-programming/restaurant/src/com/home/Kris/Chef.java
@@ -1,4 +1,18 @@
 package com.home.Kris;
 
-public class Chef {
+public class Chef implements Runnable {
+    private final String name;
+
+    public Chef(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
--- 2020-2021/second-term/object-oriented-programming/restaurant/src/com/home/Kris/Waiter.java
+++ 2020-2021/second-term/object-oriented-programming/restaurant/src/com/home/Kris/Waiter.java
@@ -1,4 +1,18 @@
 package com.home.Kris;
 
-public class Waiter {
+public class Waiter implements Runnable {
+    private final String name;
+
+    public Waiter(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
