71e7589290c2c05a9bc3f7add03605a2f34be7dd
krispetrov <petrov.kris03@gmail.com>
2021-10-04 17:24:47 +0300

    Django updated

--- 2021-2022/first-term/software-engineering/djangoWorld/HelloWorld/views.py
+++ 2021-2022/first-term/software-engineering/djangoWorld/HelloWorld/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+def main(request):
+    return redirect('home/')
+
+def home_page(request):
+    return render(request, 'home.html')
 
 # Create your views here.
