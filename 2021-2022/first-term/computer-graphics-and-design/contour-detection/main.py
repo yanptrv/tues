df840bebac7d2a5ed73c8fce0565a6251bf03219
unknown <Kristiyan Petrov>
2021-12-10 22:04:57 +0200

    Contour Detection

---
+++ 2021-2022/first-term/computer-graphics-and-design/contour-detection/main.py
@@ -0,0 +1,26 @@
+import cv2
+
+# to actually visualize the effect of `CHAIN_APPROX_SIMPLE`, we need a proper image
+image1 = cv2.imread('external-content.duckduckgo.com.jpg')
+img_gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+
+ret, thresh1 = cv2.threshold(img_gray1, 150, 255, cv2.THRESH_BINARY)
+contours2, hierarchy2 = cv2.findContours(thresh1, cv2.RETR_TREE,
+                                               cv2.CHAIN_APPROX_SIMPLE)
+image_copy2 = image1.copy()
+cv2.drawContours(image_copy2, contours2, -1, (0, 255, 0), 2, cv2.LINE_AA)
+cv2.imshow('SIMPLE Approximation contours', image_copy2)
+cv2.waitKey(0)
+image_copy3 = image1.copy()
+for i, contour in enumerate(contours2): # loop over one contour area
+   for j, contour_point in enumerate(contour): # loop over the points
+       # draw a circle on the current contour coordinate
+       cv2.circle(image_copy3, ((contour_point[0][0], contour_point[0][1])), 2, (0, 255, 0), 2, cv2.LINE_AA)
+# see the results
+cv2.imshow('CHAIN_APPROX_SIMPLE Point only', image_copy3)
+cv2.waitKey(0)
+cv2.imwrite('contour_point_simple.jpg', image_copy3)
+cv2.destroyAllWindows()
+
+
+
