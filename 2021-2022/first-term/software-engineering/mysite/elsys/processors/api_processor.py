5d647f6a67348462bc4888249a12ac78d1a3b111
Kristiyan Petrov <petrov.kris03@gmail.com>
2022-01-05 21:26:46 +0200

    Fixed json sorting, fixed mocked tests

--- 2021-2022/first-term/software-engineering/mysite/elsys/processors/api_processor.py
+++ 2021-2022/first-term/software-engineering/mysite/elsys/processors/api_processor.py
@@ -3,15 +3,31 @@ import requests
 COMMENTS_URL = "https://jsonplaceholder.typicode.com/posts/1/comments"
 POSTS_URL = "https://jsonplaceholder.typicode.com/posts"
 
+
+def sort_func(json):
+    try:
+        if 'title' in json:
+            return len(json['title'])
+        else:
+            return len(json['body'])
+    except KeyError:
+        return 0
+
+
 class ApiProcessor:
+
     @staticmethod
     def longest_comment():
         data = requests.get(COMMENTS_URL)
-        comments = sorted(data.json(), key=lambda d: d['body'], reverse=True)
+        if isinstance(data, requests.Response):
+            data = data.json()
+        comments = sorted(data, key=sort_func, reverse=True)
         return comments[0]
 
     @staticmethod
     def post_with_longest_title():
         data = requests.get(POSTS_URL)
-        posts = sorted(data.json(), key=lambda d: d['title'], reverse=True)
+        if isinstance(data, requests.Response):
+            data = data.json()
+        posts = sorted(data, key=sort_func, reverse=True)
         return posts[0]
--- 2021-2022/first-term/software-engineering/mysite/elsys/tests.py
+++ 2021-2022/first-term/software-engineering/mysite/elsys/tests.py
@@ -1,71 +1,19 @@
-import requests
 from django.test import TestCase
-from elsys.processors import api_processor
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 from elsys.processors.api_processor import ApiProcessor
 import json
 
-LONGEST_COMMENT = requests.get('https://jsonplaceholder.typicode.com/posts/1/comments').json()[2]
-LONGEST_TITLE_POST = requests.get('https://jsonplaceholder.typicode.com/posts').json()[57]
-
-
-class TestApiProcessor(TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_comment_is_longest(self):
-        assert api_processor.ApiProcessor.longest_comment() == LONGEST_COMMENT
-
-    def test_post_title_is_longest(self):
-        assert api_processor.ApiProcessor.post_with_longest_title() == LONGEST_TITLE_POST
-
-
-# from elsys.models import Car
-# from datetime import datetime
-#
-# class TestCarModel(TestCase):
-#     def setUp(self):
-#         #called when test is started
-#         Car.objects.create(color="red", made=datetime.now(), brand="audi", description = "red car", is_electric=False)
-#         Car.objects.create(color="green", made=datetime.now(), brand="bmw", description = "red bmw")
-#
-#     def tearDown(self):
-#         #called after tests were run
-#         pass
-#
-#     def test_car_is_red(self):
-#         assert Car.objects.get(brand="audi").color == "red"
-#
-#     def test_car_is_green(self):
-#         assert Car.objects.get(brand="bmw").color == "green"
-#
-#     def test_car_sound(self):
-#         assert Car.objects.get(brand="audi").sound_check() == "brum brum"
-#
-# from unittest.mock import patch
-
-# class ApiHandler:
-#    def call_api(self):
-#        response = requests.get("url")
-#        data = response['data']
-#        return data['a'] + 1
-
-
-# class TestC(TestCase):
-#    @patch("requests.get")
-#    def test_call_api(self, mocked_requests):
-#        mocked_requests.return_value = {'data':{'a': 1}}
-#        response = ApiHandler().call_api()
-#        assert response == 2
+COMMENTS_DATA = json.load(open('./elsys/comments.json'))
+POSTS_DATA = json.load(open('./elsys/posts.json'))
 
 
 class TestC(TestCase):
-    @patch("requests.get")
-    def test_call_api(self, mocked_requests):
-        data = json.load(open('./elsys/comments.json'))
-        mocked_requests.return_value.json = Mock(return_value=data)
-        response = ApiProcessor().longest_comment()
+    def test_call_longest_comment(self):
+        with patch('requests.get', return_value=COMMENTS_DATA):
+            response = ApiProcessor().longest_comment()
         assert response['id'] == 3
+
+    def test_call_longest_title(self):
+        with patch('requests.get', return_value=POSTS_DATA):
+            response = ApiProcessor().post_with_longest_title()
+        assert response['id'] == 50
