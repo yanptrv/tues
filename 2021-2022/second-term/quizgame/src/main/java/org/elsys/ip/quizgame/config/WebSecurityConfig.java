f961abd8dd71792495a0cc5e60ac37b7313bd674
Kristiyan Petrov <petrov.kris03@gmail.com>
2022-03-20 23:37:49 +0200

    Added validation for room name

---
+++ 2021-2022/second-term/quizgame/src/main/java/org/elsys/ip/quizgame/config/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package org.elsys.ip.quizgame.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/user/registration").permitAll()
+                .antMatchers("/**").authenticated()
+                .and()
+                .formLogin();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return rawPassword.toString().equals(encodedPassword);
+            }
+        };
+    }
+}
---
+++ 2021-2022/second-term/quizgame/src/main/java/org/elsys/ip/quizgame/error/RoomInvalidNameLengthException.java
@@ -0,0 +1,7 @@
+package org.elsys.ip.quizgame.error;
+
+public class RoomInvalidNameLengthException extends Exception {
+    public RoomInvalidNameLengthException(String message) {
+        super(message);
+    }
+}
